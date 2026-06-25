52eca981c42b4d43442ccbdb01eb61c979a60648
Andriy Yun <andriy.yun@gmail.com>
2026-06-25 18:26:25 +0200

    Update aliases

--- aliases/common.sh
+++ aliases/common.sh
@@ -7,6 +7,7 @@ alias rmf='rm -rf'
 
 # General
 alias k='kubectl'
+alias dea='direnv allow'
 
 # Terraform aliases
 alias tfi='terraform init'
@@ -15,9 +16,13 @@ alias tfa='terraform apply'
 alias tfd='terraform destroy'
 alias tfs='terraform show'
 alias tfv='terraform validate'
+alias tffu='terraform force-unlock'
 
-alias gca='git commit -s --amend'
+#alias gca='git commit -s --amend'
 alias grh='git reset --hard HEAD'
 alias gast='gaa && gst'
+alias gcsm='git commit -S -m'
+alias grb='git rebase -S'
+alias grbc='git rebase --continue'
 
 alias nst='netstat -nltp'
