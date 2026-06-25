5d2135d17034f08766be11fca93ff5b33367ee90
Andriy Yun <andriy.yun@gmail.com>
2026-06-25 18:26:49 +0200

    Remove old nix config

--- nix.sh
+++
@@ -1,4 +0,0 @@
-eval "$(direnv hook zsh)"
-if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by
-
-# See NIX aliases in separate aliaes.sh file.
